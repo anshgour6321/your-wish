97860b2f01ab58f0560bcae7f38fe89192bc3219
anshgour <anshgour136@gmail.com>
2024-01-12 02:02:31 +0530

    hlo

---
+++ script.js
@@ -0,0 +1,8 @@
+let h1=document.querySelector(".h1");
+function clr(){
+    for(let i=0; i<btn.length; i++){
+        btn[i].remove();
+    }
+h1.innerHTML="Love u...💛";
+}
+let btn= document.querySelectorAll(".btn");
